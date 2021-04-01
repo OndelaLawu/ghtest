c40dc7c1b36a70d28448700439e215af535d83c6
simnikiwe <simnikiwe@Hp250>
2021-04-01 10:17:56 +0200

    First Commit

---
+++ src/main/java/za/ac/cput/ghtest/DisplayCode.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.ghtest;
+
+/**
+ *
+ * @author Kevin Mombo
+ * 
+ * PLEASE ENTER YOUR CODE BELOW!
+ */
+
+public class DisplayCode{
+/**
+ *
+ * @author Ondela Lawu
+ * 
+ * Added the method
+ */
+}
+public class DisplayCode {
+    
+}
